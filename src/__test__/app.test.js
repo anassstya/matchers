65d68ca4550c059b4a0d6a17951039e59b5b2a5d
anassstya <408183@edu.itmo.ru>
2024-08-29 16:40:18 +0900

    add webpack

---
+++ src/__test__/app.test.js
@@ -0,0 +1,19 @@
+import sort from '../app.js';
+
+test('should sort objects', () => {
+  let a = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  
+  const result = sort(a);
+
+  const answer = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
+  expect(result).toEqual(answer);  // Используем toEqual для сравнения значений объектов
+});
--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ export default function selection_sort(arr){
     for(let i = 0; i<arr.length; i++){
         let smallest = i;
         for(let j = i+1; j<arr.length; j++){
-            if(arr[smallest].health > arr[j].health){
+            if(arr[smallest].health < arr[j].health){
                 smallest = j;
             }
         }
@@ -12,4 +12,3 @@ export default function selection_sort(arr){
     }
     return arr;
 }
-
